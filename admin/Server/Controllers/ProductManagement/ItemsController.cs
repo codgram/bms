d80c720d667ca6ed3118f40bb945728b6e05e1a4
Kevork Keheian <kevork.keheian@gmail.com>
2022-11-23 00:19:37 +0200

    api: add items controller

---
+++ admin/Server/Controllers/ProductManagement/ItemsController.cs
@@ -0,0 +1,109 @@
+#nullable disable
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Application.Model.ProductManagement;
+using Application.Server.Data;
+
+namespace Application.Server.Controllers.ProductManagement
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ItemsController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+
+        public ItemsController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Item
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Item>>> GetItems([FromQuery] string companyId)
+        {
+            return await _context.Item.Include(i => i.Subgroup).Where(i => i.CompanyId == companyId).ToListAsync();
+        }
+
+        // GET: api/Item/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Item>> GetItem(string id)
+        {
+            var item = await _context.Item.FindAsync(id);
+
+            if (item == null)
+            {
+                return NotFound();
+            }
+
+            return item;
+        }
+
+        // PUT: api/Item/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutItem(string id, Item item)
+        {
+            if (id != item.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(item).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!ItemExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Item
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<Item>> PostItem(Item item)
+        {
+            _context.Item.Add(item);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetItem", new { id = item.Id }, item);
+        }
+
+        // DELETE: api/Item/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteItem(string id)
+        {
+            var item = await _context.Item.FindAsync(id);
+            if (item == null)
+            {
+                return NotFound();
+            }
+
+            _context.Item.Remove(item);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool ItemExists(string id)
+        {
+            return _context.Item.Any(e => e.Id == id);
+        }
+    }
+}
