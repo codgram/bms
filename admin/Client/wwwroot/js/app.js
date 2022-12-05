16fc4939c5e1034715b1a006a178280d67cd72fd
Kevork Keheian <kevork.keheian@gmail.com>
2022-12-05 13:29:53 +0200

    fix: update the export function - the user was being redirected to the export page (api) - change from navigation to js function

--- admin/Client/wwwroot/js/app.js
+++ admin/Client/wwwroot/js/app.js
@@ -1,9 +1,19 @@
-// create saveAsFile function that get the file name and the file content and data
-function saveAsFile(fileName, fileContent, data) {
-    var blob = new Blob([fileContent], { type: "text/plain;charset=utf-8" });
+function saveAsFile(filename, contentType, data) {
 
-    //add data
-    var data = data;
-    
-    saveAs(blob, fileName);
+    // Create the URL
+    const file = new File([data], filename, { type: contentType });
+    const exportUrl = URL.createObjectURL(file);
+
+    // Create the <a> element and click on it
+    const a = document.createElement("a");
+    document.body.appendChild(a);
+    a.href = exportUrl;
+    a.download = filename;
+    a.target = "_self";
+    a.click();
+
+    // We don't need to keep the url, let's release the memory
+    // On Safari it seems you need to comment this line... (please let me know if you know why)
+    URL.revokeObjectURL(exportUrl);
+    a.remove();
 }
